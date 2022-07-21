783b582e5ea33ec5ee13443346878f09e830bb5a
AMIT <86924251+amitxv@users.noreply.github.com>
2022-07-21 15:20:12 +0100

    enable delayed expansion

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 :: Requirements
 ::
@@ -6,51 +7,51 @@
 :: - 7-Zip
 
 set "path_err=0"
-for %%i in (
+for %%a in (
     "python.exe",
     "pip.exe",
 ) do (
-    where %%i
-    if not %errorlevel% == 0 (
+    where %%a
+    if not !errorlevel! == 0 (
         set "path_err=1"
-        echo error: %%i not found in path
+        echo error: %%a not found in path
     )
 )
-if not %path_err% == 0 exit /b
+if not !path_err! == 0 exit /b
 
 set "CURRENT_DIR=%~dp0"
-set "CURRENT_DIR=%CURRENT_DIR:~0,-1%"
+set "CURRENT_DIR=!CURRENT_DIR:~0,-1!"
 
-set "BUILD_ENV=%CURRENT_DIR%\BUILD_ENV"
-set "PROJECT_DIR=%BUILD_ENV%\main"
-set "PUBLISH_DIR=%BUILD_ENV%\win-wallpaper"
+set "BUILD_ENV=!CURRENT_DIR!\BUILD_ENV"
+set "PROJECT_DIR=!BUILD_ENV!\main"
+set "PUBLISH_DIR=!BUILD_ENV!\win-wallpaper"
 
-if exist "%BUILD_ENV%" (
-    rd /s /q "%BUILD_ENV%"
+if exist "!BUILD_ENV!" (
+    rd /s /q "!BUILD_ENV!"
 )
-mkdir "%BUILD_ENV%"
-mkdir "%PROJECT_DIR%"
+mkdir "!BUILD_ENV!"
+mkdir "!PROJECT_DIR!"
 
-python -m venv "%BUILD_ENV%"
-call "%BUILD_ENV%\Scripts\activate.bat"
+python -m venv "!BUILD_ENV!"
+call "!BUILD_ENV!\Scripts\activate.bat"
 
 pip install -r requirements.txt
 
-copy /y "%CURRENT_DIR%\win-wallpaper.py" "%PROJECT_DIR%"
-cd "%PROJECT_DIR%"
+copy /y "!CURRENT_DIR!\win-wallpaper.py" "!PROJECT_DIR!"
+cd "!PROJECT_DIR!"
 
 pyinstaller "win-wallpaper.py" --onefile --uac-admin
 
-call "%BUILD_ENV%\Scripts\deactivate.bat"
+call "!BUILD_ENV!\Scripts\deactivate.bat"
 
-cd "%CURRENT_DIR%"
+cd "!CURRENT_DIR!"
 
 if exist "win-wallpaper.exe" (
     del /f /q "win-wallpaper.exe"
 )
 
-move "%PROJECT_DIR%\dist\win-wallpaper.exe" "%CURRENT_DIR%"
+move "!PROJECT_DIR!\dist\win-wallpaper.exe" "!CURRENT_DIR!"
 
-rd /s /q "%BUILD_ENV%"
+rd /s /q "!BUILD_ENV!"
 
 exit /b
