3414aae632860fb2b8566aa3e93a54aa807df8eb
amitxv <amitxv@users.noreply.github.com>
2023-01-20 12:47:51 +0000

    process images in parallel

--- win-wallpaper.py
+++ win-wallpaper.py
@@ -1,17 +1,32 @@
 import glob
 import subprocess
-import sys
 import os
 import argparse
 import ctypes
+import multiprocessing
 from PIL import Image, ImageColor
 
 
-def main() -> int:
-    """program entrypoint"""
+stdnull = {"stdout": subprocess.DEVNULL, "stderr": subprocess.DEVNULL}
 
+
+def modify_image(image_path, rgb_value):
+    # take ownership of the images
+    subprocess.run(["takeown", "/F", image_path, "/A"], check=False, **stdnull)
+    subprocess.run(["icacls", image_path, "/grant", "Administrators:F"], check=False, **stdnull)
+
+    try:
+        with Image.open(image_path) as original_image:
+            size = original_image.size
+
+        with Image.new("RGB", size, rgb_value) as new_image:
+            new_image.save(image_path)
+    except PermissionError:
+        print(f"error: permission error accessing {image_path}")
+
+
+def main():
     version = "0.3.3"
-    subprocess_null = {"stdout": subprocess.DEVNULL, "stderr": subprocess.DEVNULL}
     images = []
 
     print(f"win-wallpaper v{version}")
@@ -19,12 +34,10 @@ def main() -> int:
 
     if not ctypes.windll.shell32.IsUserAnAdmin():
         print("error: administrator privileges required")
-        return 1
+        return
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--version", action="version", version=f"win-wallpaper v{version}"
-    )
+    parser.add_argument("--version", action="version", version=f"win-wallpaper v{version}")
     parser.add_argument(
         "--dir",
         metavar="<directory>",
@@ -42,59 +55,48 @@ def main() -> int:
     parser.add_argument("--win7", action="store_true", help="enables Windows 7 support")
     args = parser.parse_args()
 
-    image_paths = [
+    image_paths = (
         f"{args.dir}\\ProgramData\\Microsoft\\User Account Pictures",
         f"{args.dir}\\Windows\\Web",
         f"{args.dir}\\ProgramData\\Microsoft\\Windows\\SystemData",
-    ]
+    )
 
-    if not any(os.path.exists(x) for x in image_paths):
+    if not any(os.path.exists(path) for path in image_paths):
         print("error: no folders found, invalid directory")
-        return 1
+        return
 
     try:
         rgb_value = tuple(ImageColor.getcolor(args.rgb, "RGB"))
     except ValueError:
         print("error: invalid hex code for --rgb argument")
-        return 1
+        return
 
     for folder_path in image_paths:
-        for file_type in ["jpg", "png", "bmp"]:
-            for i in glob.glob(f"{folder_path}/**/*.{file_type}", recursive=True):
-                if i not in images:
-                    images.append(i)
+        for file_type in ("jpg", "png", "bmp"):
+            for image in glob.glob(f"{folder_path}/**/*.{file_type}", recursive=True):
+                if image not in images:
+                    images.append(image)
 
-    for image in images:
-        # take ownership of the images
-        subprocess.run(["takeown", "/F", image, "/A"], check=False, **subprocess_null)
-        subprocess.run(
-            ["icacls", image, "/grant", "Administrators:F"],
-            check=False,
-            **subprocess_null,
-        )
-
-        try:
-            original = Image.open(image)
-            new = Image.new("RGB", original.size, rgb_value)
-            new.save(image)
-        except PermissionError:
-            print(f"error: permission error accessing {image}")
+    pool_args = [(image, rgb_value) for image in images]
+    with multiprocessing.Pool() as pool:
+        pool.starmap(modify_image, pool_args)
 
     if args.win7:
         oobe_background_path = f"{args.dir}\\Windows\\System32\\oobe\\info\\backgrounds"
         os.makedirs(oobe_background_path, exist_ok=True)
         image = f"{oobe_background_path}\\backgroundDefault.jpg"
+
         try:
-            new = Image.new("RGB", (1920, 1080), rgb_value)
-            new.save(image)
+            with Image.new("RGB", (1920, 1080), rgb_value) as new_image:
+                new_image.save(image)
         except PermissionError:
             print(f"error: permission error accessing {image}")
-            return 1
+            return
 
     print("info: done")
 
-    return 0
+    return
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
