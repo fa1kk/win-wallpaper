e1a8823c8fe90baaaf6473bfc31df7be86b55f8a
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-06 16:51:26 +0000

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,20 @@
+function main() {
+    if (Test-Path ".\build\") {
+        Remove-Item -Path ".\build\" -Recurse
+    }
+
+    # entrypoint relative to .\build\pyinstaller\
+    $entryPoint = "..\..\win_wallpaper\main.py"
+
+    # pack executable
+    New-Item -ItemType Directory -Path ".\build\pyinstaller\"
+    Push-Location ".\build\pyinstaller\"
+    pyinstaller $entryPoint --onefile --name win-wallpaper
+    Pop-Location
+
+    return 0
+}
+
+$_exitCode = main
+Write-Host # new line
+exit $_exitCode
